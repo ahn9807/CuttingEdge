88baf22995a9df6c20759c069ac492765eb3ae95
Ahn Junho <ahn9807@gmail.com>
2020-01-06 23:00:44 +0900

    1/6

---
+++ app/src/main/java/com/example/cuttingedge/ChatData.java
@@ -0,0 +1,8 @@
+package com.example.cuttingedge;
+
+public class ChatData {
+    public String id;
+    public String nickname;
+    public String date;
+    public String message;
+}
---
+++ app/src/main/java/com/example/cuttingedge/ChattingActivity.java
@@ -0,0 +1,14 @@
+package com.example.cuttingedge;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChattingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+    }
+}
